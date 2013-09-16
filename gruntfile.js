a985234e5554d05b87e5a2ffe613de0fda58dbe9
Bryan Grimes <bryan@bryangrimes.com>
2013-09-15 22:08:58 -0400

    working

--- gruntfile.js
+++ gruntfile.js
@@ -86,6 +86,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-nodemon');
     grunt.loadNpmTasks('grunt-concurrent');
+    //grunt.loadNpmTasks("grunt-modernizr");
 
     //Making grunt default to force in order not to break the project.
     grunt.option('force', true);
